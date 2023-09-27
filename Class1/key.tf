3e015d00ad13649987967076833299cf02e307a1
dennian123 <den.ianovskyi@gmail.com>
2023-09-27 21:54:31 +0000

    new files

--- Class1/key.tf
+++ Class1/key.tf
@@ -4,5 +4,16 @@ resource "aws_key_pair" "deployer" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "dennis-kk-123"
+  bucket_prefix = "hello-"
+}
+resource "aws_s3_bucket" "bucket" {
+  bucket = "dennis-kk1"
+}
+
+
+output bucket_info {
+  value = [
+    aws_s3_bucket.bucket.bucket_domain_name,
+    aws_s3_bucket.bucket.arn,
+  ]
 }
